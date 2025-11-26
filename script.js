7fe11a4f0b0cbbb4395365e9adb9344dbce83497
claudiorezende-cyber <claudio.rezende@escola.pr.gov.br>
2025-11-26 17:04:34 -0300

    professora1

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 
-
 avanca.forEach(button => {
-button.addEventListener('click', () => {
-const atual = document.querySelector('.ativo');
-const proximoPasso = 'passo-' + button.getAttribute('data-proximo');
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-
-atual.classList.remove('ativo');
-document.getElementById(proximoPasso).classList.add('ativo');
-});
-});
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
